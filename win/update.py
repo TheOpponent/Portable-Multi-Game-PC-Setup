c114ba08e6451ee013f7f083fa1a290f0061937e
The Opponent <theo6272@minuspoint.com>
2026-08-02 00:59:54 -0400

    update.py: Use `publish.single()` instead

--- win/update.py
+++ win/update.py
@@ -18,17 +18,13 @@ BROKER_IP = "192.168.19.1"
 BROKER_PORT = 1883
 
 def main():
-    msgs = []
-
     if len(sys.argv) == 1:
-        msgs.append(("lutero/sign", "^reset", 2))
+        publish.single("lutero/sign", "^reset", 2, False, BROKER_IP, BROKER_PORT)
     elif len(sys.argv) == 2:
-        msgs.append(("lutero/sign", sys.argv[1], 2))
+        publish.single("lutero/sign", sys.argv[1], 2, False, BROKER_IP, BROKER_PORT)
     else:
         print("Incorrect number of arguments.")
         sys.exit()
-
-    publish.multiple(msgs, BROKER_IP, BROKER_PORT)
 
 
 if __name__ == "__main__":
